46b444bedb431f3edcc635212c88b9cbdf483040
Ivan Volkovets <batmad857@gmail.com>
2022-09-14 14:17:04 +0300

    create base structure

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
   <div>
-    <button @:click="addLike">Like</button>
-    <button @:click="addDisLike">Dislike</button>
-    <div>Total likes: {{likes}}</div>
-    <div>Total dislikes: {{dislikes}}</div>
+    
   </div>
 </template>
 
 <script>
 export default {
     data(){
-        return {
-            likes:0,
-            dislikes:0,
-        }
+
     },
     methods: {
-        addLike(){
-            this.likes +=1;
-        },
-        addDisLike(){
-            this.dislikes +=1;
-        }
+        
     }
 }
 </script>
