60ebbf5f4ff6a0153577a6b6b2221e8c996ce66a
Ivan Volkovets <batmad857@gmail.com>
2022-09-14 17:04:35 +0300

    add styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    
+    <div class="post">
+        <div><strong>Title:</strong>Javascript post</div>
+        <div><strong>Title:</strong>Description</div>
+    </div>
+    <div class="post">
+        <div><strong>Title:</strong>Javascript post</div>
+        <div><strong>Title:</strong>Description</div>
+    </div>
   </div>
 </template>
 
@@ -15,6 +22,16 @@ export default {
 }
 </script>
 
-<style>
+<style >
+    *{
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+    }
 
+    .post{
+        padding: 1rem;
+        border: 0.1rem solid darkseagreen;
+        margin-top: 1rem;
+    }
 </style>
