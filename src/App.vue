522a909a8ad8421a4e60a43180ed1215e1beb391
Ivan Volkovets <batmad857@gmail.com>
2022-09-15 17:14:49 +0300

    add createPost function

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <form>
+    <form  @submit.prevent>
         <h4>Create Post</h4>
         <input 
             v-bind:value="title" 
@@ -40,11 +40,15 @@ export default {
     },
     methods: {
         createPost(){
-
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost)
+            this.title = ''
+            this.body = ''
         },
-        inputTitle(event){
-            this.title = event.target.value;
-        }
     }
 }
 </script>
