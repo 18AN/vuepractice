27ea799c75fd1af14075b03a8a83981bb23c28ba
unknown <ivan.v@allsports.by>
2022-10-03 16:58:09 +0300

    move PostForm component into a modal window

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,62 @@
 <template>
   <div class="app">
-    <post-form @create="createPost"/>
-    <post-list 
-        :posts="posts"
-        @deletePost="deletePost"
-    />
+    <h1>Страница с постами</h1>
+    <my-button @click="showDialog">Создать пост</my-button>
+    <my-form-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-form-dialog>
+    <post-list :posts="posts" @deletePost="deletePost" />
   </div>
 </template>
 
 <script>
-import PostForm from '@/components/PostForm.vue'
-import PostList from '@/components/PostList.vue'
+import PostForm from "@/components/PostForm.vue";
+import PostList from "@/components/PostList.vue";
+import MyFormDialog from "./UI/MyFormDialog.vue";
+import MyButton from "./UI/MyButton.vue";
 export default {
-    components: {
-        PostForm, PostList
+  components: {
+    PostForm,
+    PostList,
+    MyFormDialog,
+    MyButton,
+  },
+
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "Javascript", body: "Description" },
+        { id: 2, title: "Javascript 2", body: "Description 2" },
+        { id: 3, title: "Javascript 3", body: "Description 3" },
+      ],
+      dialogVisible: false,
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
     },
-    data(){
-        return{
-            posts: [
-                {id: 1, title: 'Javascript', body: 'Description'},
-                {id: 2, title: 'Javascript 2', body: 'Description 2'},
-                {id: 3, title: 'Javascript 3', body: 'Description 3'},
-            ],
-        }  
+
+    deletePost(post) {
+      this.posts = this.posts.filter((item) => item.id !== post.id);
     },
-    methods: {
-        createPost(post){
-            this.posts.push(post)
-        },
-        
-        deletePost(post){
-            this.posts = this.posts.filter(item => item.id !== post.id)
-        }
-    }
-}
+
+    showDialog() {
+      this.dialogVisible = true;
+    },
+  },
+};
 </script>
 
 <style>
-    *{
-        margin: 0px;
-        padding: 0px;
-        box-sizing: border-box;
-    }
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
 
-    .app{
-        padding: 2rem;
-    }
+.app {
+  padding: 2rem;
+}
 </style>
